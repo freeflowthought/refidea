3475ab68ac1e38b6948e7d91e43f094bc0e7a97c
freeflowthought <atlasia.project@gmail.com>
2022-11-30 17:45:11 -0500

    add the API endpoint documentation tool- swagger

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     //prevent user to inject the field which doesn't exists
+  const configService = app.get(ConfigService);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('API with NestJS')
+    .setDescription('API developed throughout the API with NestJS course')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-  await app.listen(3333);
+  const port = configService.get('PORT') ?? 3000;
+  await app.listen(port);
 }
 bootstrap();
